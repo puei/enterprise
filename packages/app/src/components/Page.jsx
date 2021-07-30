7b7c579b0c74197b42a20afb4d86d55c8d70b965
puei <dyj2017_fqd@163.com>
2021-07-30 17:01:04 +0800

    12

--- packages/app/src/components/Page.jsx
+++ packages/app/src/components/Page.jsx
@@ -1,9 +1,25 @@
 import styled from '@emotion/styled'
+import { useEffect } from 'react'
 
 const Page = styled.main`
-    flex:1;
-    display:flex;
-    flex-direction:column;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
 `
 
-export default Page
+function PageTitle({ children }) {
+    if (!(typeof children === 'string')) {
+        throw new Error(`${children} is not a string.`)
+    }
+
+    useEffect(
+        function setTitle() {
+            document.title = children
+        },
+        [children]
+    )
+
+    return null
+}
+
+export { PageTitle, Page }
---
+++ packages/app/src/globalComponents.js
@@ -0,0 +1,6 @@
+import GlobalCSS from './components/GlobalCSS'
+
+
+
+/** 全局组件 */
+export default [GlobalCSS]
